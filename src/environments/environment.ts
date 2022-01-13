1faa7333b0d6a349db7fb4412e693f19ef16851a
johrakhan360 <88780561+johrakhan360@users.noreply.github.com>
2022-01-13 13:49:02 +0530

    changes resolved

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,7 @@ export const environment = {
 
 // domain:'http://Nimai-Prd-LB-1296056161.ap-south-1.elb.amazonaws.com' ,
 // support:'tradesupport@360tf.trade',
-//----------Dev server-----------------
+//----------Dev server--------------------
 
 domain:'http://136.232.244.190:8081',
 support:'dev-tradesupport@360tf.trade', 
